e55ba74ebb87165bccf8814018638372feca3cf9
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:47:15 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split('=');
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word}");
+            }
+        }
+    }    
+}
